794061578a532944a97711c074845a0f0adf47a5
Rodrigo Villalba <rodris.jvo@gmail.com>
2017-07-20 16:18:42 -0400

    We remove no interesting tests for our scenario

--- app/numbers.js
+++ app/numbers.js
@@ -1,17 +1,6 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
-
-  },
-
-  base10: function(str) {
-
-  },
-
-  convertToBinary: function(num) {
-
-  },
 
   multiply: function(a, b) {
 
