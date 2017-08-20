b92c12318e88bff8913422c5d84078f43df2fb2e
Mirta Gonzalez <mirti.gonz@gmail.com>
2017-08-19 20:11:11 -0400

    Agregar funciones sync
    Agrega la funcion async.
    
    No pude implementar manipulateRemoteData, por
    alguna razon me perdi para retornar promise.

--- app/async.js
+++ app/async.js
@@ -2,10 +2,14 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    var promise = new Promise(function(solve, error){
+        solve(value);
+        error('Error');
+    })
+    return promise;
   },
 
   manipulateRemoteData: function(url) {
-
+  
   }
 };
