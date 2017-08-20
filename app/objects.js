cad12e15d6171d3c2dd41dcf80b21775121f5633
Mirta Gonzalez <mirti.gonz@gmail.com>
2017-08-19 20:18:21 -0400

    Agrega funciones referentes a object

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+   return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+      var properties = [];
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          var element = obj[key];
+          properties.push(key+': '+element);
+        }
+      }
+      return properties;
   }
 };
