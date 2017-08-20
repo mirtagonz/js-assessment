e2fc3acc3ecee37781d543e2d3199d49d28ff432
Mirta Gonzalez <mirti.gonz@gmail.com>
2017-08-19 20:18:57 -0400

    Agrega funciones de string solicitadas
    Los primeros dos solicitados creo que se
    puede hacer con expresiones regulares,
    pero me di cuenta que hace rato que no las
    hago y tengo que volver a leer.
    
    La ultima "reverseString", las hice jugando
    con array.

--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,14 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+   //con expresion regular
   },
 
   wordWrap: function(str, cols) {
-
+    //con expresion regular
   },
 
   reverseString: function(str) {
-
+    return str.split("").reverse().join("");
   }
 };
